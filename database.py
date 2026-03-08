c9075cd637dac3d1d64880e844ce8671b5829027
stefan-olarnic <stefanolarnic@gmail.com>
2026-03-08 13:24:59 +0200

    debug database url

--- database.py
+++ database.py
@@ -2,13 +2,22 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+
+def get_engine():
+    database_url = os.getenv("DATABASE_URL")
+    print(f"DATABASE_URL found: {database_url is not None}")
+    if not database_url:
+        raise ValueError("DATABASE_URL environment variable is not set")
+    return create_engine(database_url)
+
+
+engine = get_engine()
+SessionLocal = sessionmaker(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
